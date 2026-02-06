9c674ad79a57e8b8904c1a61a21e209ddf8bbfa8
Yosephjgb <cjadaleon@gmail.com>
2026-02-06 23:25:12 +0300

    first simple easy typing game

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ const textInput = document.getElementById("textInput");
 const scoreDisplay = document.getElementById("score");
 const timeDisplay = document.getElementById("time");
 
+let score = 0;
+let time = 10;
+let currentWord = "";
+
+function showWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    currentWord = words[randomIndex];
+    wordDisplay.textContent = currentWord;
+}
+
+function updateScore() {
+    score++;
+    scoreDisplay.textContent = score;
+}
+
+function countdown(){
+    time--;
+    timeDisplay.textContent = time;
+    
+    if (time === 0) {
+        clearInterval(timer);
+        alert("Game Over! Your score: " + score);
+    }
+}
+
+textInput.addEventListener("input", () => {
+    if (textInput.value === currentWord) {
+        updateScore();
+        showWord();
+        textInput.value = "";
+        time += 2;
+    }
+});
+
+showWord();
+
+const timer = setInterval(countdown, 1000); 
