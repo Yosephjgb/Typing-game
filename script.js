96780c55faa7d95e66c1c9f05d5464fdfb5c26f7
Jgamerboy <cjadaleon@gmail.com>
2026-02-06 22:50:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const words = ["apple", "banana", "computer", "keyboard", "game", "javascript", "programming", "challenge", "developer", "function"];
+
+const wordDisplay = document.getElementById("word");
+const textInput = document.getElementById("textInput");
+const scoreDisplay = document.getElementById("score");
+const timeDisplay = document.getElementById("time");
+
