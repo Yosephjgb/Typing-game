650062961c37d77d21895b0044875bb384f79c1a
Yosephjgb <cjadaleon@gmail.com>
2026-02-09 00:16:12 +0300

    New Interface

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
-const words = ["apple", "banana", "computer", "keyboard", "game", "javascript", "programming", "challenge", "developer", "function"];
+// Display Username
+const playerName = document.getElementById("playerName");
 
-const wordDisplay = document.getElementById("word");
-const textInput = document.getElementById("textInput");
-const scoreDisplay = document.getElementById("score");
-const timeDisplay = document.getElementById("time");
+const username = localStorage.getItem("username");
 
-let score = 0;
-let time = 10;
-let currentWord = "";
-
-function showWord() {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    currentWord = words[randomIndex];
-    wordDisplay.textContent = currentWord;
+if (username) {
+    playerName.textContent = username;
 }
 
-function updateScore() {
-    score++;
-    scoreDisplay.textContent = score;
+
+// Dark Theme Toggle
+const toggleBtn = document.getElementById("themeToggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark");
 }
 
-function countdown(){
-    time--;
-    timeDisplay.textContent = time;
-    
-    if (time <= 0) {
-        clearInterval(timer);
-        endGame();}
-}
+toggleBtn.addEventListener("click", () => {
 
-textInput.addEventListener("input", () => {
-    if (textInput.value === currentWord) {
-        updateScore();
-        showWord();
-        textInput.value = "";
-        time += 2;
-    }
+    document.body.classList.toggle("dark");
+
+    const currentTheme = document.body.classList.contains("dark")
+        ? "dark"
+        : "light";
+
+    localStorage.setItem("theme", currentTheme);
 });
-
-function endGame(){
-    clearInterval(timer);
-
-    textInput.disabled = true;
-
-    document.getElementById("finalScore").textContent = score;
-    document.getElementById("gameOver").classList.remove("hidden");
-}
-showWord();
-
-const timer = setInterval(countdown, 1000); 
